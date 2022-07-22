d2a0a31a2d5487a00a63d077a9eadf3db7f30dad
StarFrost <109770359+IcyStarFrost@users.noreply.github.com>
2022-07-21 19:42:18 -0500

    Update sounds.lua

--- lua/zeta/sounds.lua
+++ lua/zeta/sounds.lua
@@ -21,7 +21,6 @@ function ENT:EmitStepSound(level, volume)
     end
 end
 
---test
 function ENT:ZetaPlayVoiceSound(sndName, callRemove, stopPlaying)
 	if stopPlaying and (self.IsSpeaking == true or self.AllowVoice == false) then return end
 	self.IsSpeaking = true
